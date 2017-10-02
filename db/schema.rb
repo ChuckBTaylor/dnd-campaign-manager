001ab1775ddabd49c30abb7eab7e75c3afa049ee
ChuckBTaylor <WLScripps@gmail.com>
2017-10-02 14:51:32 -0400

    Take me

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171002173208) do
 
   create_table "campaigns", force: :cascade do |t|
