e0c293bbdf7bfff76dfb54face31bdf5ef695d41
ChuckBTaylor <WLScripps@gmail.com>
2017-10-04 16:37:00 -0400

    seed stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 x = Api.new
 x.populate_table("spell")
+x.populate_class_name
+x.populate_class_names_spells
 x.populate_race_info
 Race.add_manually_entered_races
 x.populate_table("trait")
-x.populate_class_name
 Campaign.create(name:"No Campaign",user_id:1)
